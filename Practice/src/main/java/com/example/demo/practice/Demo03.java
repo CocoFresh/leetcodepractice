07e8598e0496328c845097bd3569c7d8e51fd116
xintao <291954264@qq.com>
2018-09-12 17:54:16 +0800

    leetcode练习

--- Practice/src/main/java/com/example/demo/practice/Demo03.java
+++ Practice/src/main/java/com/example/demo/practice/Demo03.java
@@ -1,64 +1,14 @@
-package leetcode;
+package com.example.demo.practice;
 
 /**
  * @author xintao.feng
- * @date 2018/9/10.
+ * @date 2018/9/12.
  */
-public class solution {
+public class Demo03 {
 
     private Integer INT_MAX = 2147483647;
     private Integer INT_MIN = -2147483648;
-
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j]) {
-
-                    result[0] = nums[i];
-                    result[1] = nums[j];
-                }
-            }
-        }
-        return result;
-    }
-
-    public int reverse(int x) {
-//        int rev = 0;
-//        while (x != 0) {
-//            int pop = x % 10;
-//            x /= 10;
-//            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
-//            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
-//            rev = rev * 10 + pop;
-//        }
-//        return rev;
-        char symbol = '-';
-        Integer y = x;
-        String m = y.toString();
-        char[] num = new char[m.length()];
-        String n = new String();
-        for (int i = 0; i < num.length; i++) {
-            num[i] = m.charAt(i);
-        }
-        if (x < 0) {
-            n += symbol;
-            for (int i = num.length - 1; i >= 1; i--) {
-                n += num[i];
-            }
-        } else {
-            for (int i = num.length - 1; i >= 0; i--) {
-                n += num[i];
-            }
-        }
-        Long y1 = Long.parseLong(n);
-        if (y1 > INT_MAX || y1 < INT_MIN) {
-            return 0;
-        }
-        y = Integer.parseInt(n);
-        return y;
-    }
-//字符串转整数
+    //字符串转整数
     public int myAtoi(String str) {
         String n = "";
         if (str.equals(n) || str.equals("-")) {
@@ -94,7 +44,7 @@ public class solution {
         }
         try {
             return Integer.parseInt(n);
-        }catch (Exception e){
+        } catch (Exception e) {
             if (n.charAt(0) == '-') {
                 if (n.length() > INT_MIN.toString().length()) {
                     return INT_MIN;
@@ -115,21 +65,4 @@ public class solution {
         }
         return Integer.parseInt(n);
     }
-
-    public static void main(String[] args) {
-
-
-        solution solution = new solution();
-//        int[] nums = {2, 7, 11, 15};
-//        int target = 9;
-//        int[] result = solution.twoSum(nums,target);
-//        System.out.println(
-//                result
-//        );
-        int x = solution.myAtoi("    000000000000000    ");
-//        int x = solution.reverse(-2147483648);
-        System.out.println(x);
-    }
-
 }
-
---
+++ Practice/src/main/java/com/example/demo/practice/Demo08.java
@@ -0,0 +1,20 @@
+package com.example.demo.practice;
+
+/**
+ * @author xintao.feng
+ * @date 2018/9/12.
+ */
+public class Demo08 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
