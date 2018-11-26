4001a5990cdfe4905978929e6d3768d39c06a8b9
xintao <291954264@qq.com>
2018-11-26 09:40:36 +0800

    11.26

---
+++ Practice/src/main/java/com/example/demo/practice/Demo54.java
@@ -0,0 +1,31 @@
+package com.example.demo.practice;
+
+import com.example.demo.practice.common.ListNode;
+
+/**
+ * 回文链表
+ */
+public class Demo54 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode p = head;
+        int len = 0;
+        while (p != null) {
+            p = p.next;
+            len++;
+        }
+        int[] num = new int[len];
+        int i = 0;
+        p = head;
+        while (p!=null){
+            num[i] = p.val;
+            p = p.next;
+            i++;
+        }
+        for (int j = 0; j < num.length/2; j++) {
+            if (num[j] != num[num.length-1-j]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
